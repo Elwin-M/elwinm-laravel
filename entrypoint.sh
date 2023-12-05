fbf32c7c093d7aa928c1eaa4b04cf5b7b29d2752
Elwin <elwin-moolakkattu@live.ca>
2023-12-05 13:30:08 -0500

    from update to install

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Check if node_modules directory exists, if not, install dependencies
+# Check if node_modules directory exists, if not, install Node.js dependencies
 if [ ! -d "/var/www/html/node_modules" ]; then
-    echo "Node modules not found. Installing dependencies..."
+    echo "Node modules not found. Installing Node.js dependencies..."
     npm install
 fi
 
@@ -12,10 +12,10 @@ if [ ! -f "/var/www/html/package-lock.json" ]; then
     npm install
 fi
 
-# Check if vendor directory exists, if not, install dependencies
+# Check if vendor directory exists, if not, install PHP dependencies using Composer
 if [ ! -d "/var/www/html/vendor" ]; then
-    echo "Vendor not found. Installing dependencies..."
-    composer update
+    echo "Vendor directory not found. Installing PHP dependencies..."
+    composer install
 fi
 
 # Start the main process
