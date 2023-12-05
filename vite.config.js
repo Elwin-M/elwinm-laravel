4f711e9b1be808d433c6a245c7330ae5a03ad402
Elwin <elwin-moolakkattu@live.ca>
2023-12-05 13:30:16 -0500

    removed polling

--- vite.config.js
+++ vite.config.js
@@ -19,8 +19,8 @@ export default defineConfig({
         hmr: {
             host: 'localhost'
         },
-        watch: {
-            usePolling: true,
-        },
+        // watch: {
+        //     usePolling: true,
+        // },
     }
 });
