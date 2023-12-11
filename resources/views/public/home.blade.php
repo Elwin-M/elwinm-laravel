f1d887d268e20ddc712639720f87c971b2ea0167
Elwin <elwin-moolakkattu@live.ca>
2023-12-11 18:42:25 -0500

    removed old pages

--- resources/views/public/home.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layouts.public>
-    @section('pageTitle', 'Elwin\'s Home')
-
-    <body class="antialiased bg-slate-900">
-        <x-modules.nav-bar />
-        <x-modules.hero-section />
-        <x-modules.filler />
-    </body>
-</x-layouts.public>
