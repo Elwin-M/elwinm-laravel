31d1d6eee6e6ed64f32d46676b8039fc63c5ffe9
Elwin <elwin-moolakkattu@live.ca>
2023-12-12 16:10:21 -0500

    404 pages

---
+++ resources/views/404.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.app>
+    <!-- Nav bar and menu -->
+    <x-modules.nav-bar />
+    <x-modules.mobile-menu />
+    <!-- Hero section -->
+    <x-modules.hero-section />
+</x-layouts.app>
---
+++ resources/views/pages/404.blade.php
@@ -0,0 +1,9 @@
+<!-- Home page -->
+<!-- Setting the title -->
+{{-- @section('pageTitle')
+'Elwin\'s 123'
+@endsection --}}
+
+<div>
+    404
+</div>
