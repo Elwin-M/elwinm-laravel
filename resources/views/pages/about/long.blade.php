20a5f2bbf0322b596e184e3d26b6bcb890049aec
Elwin <elwin-moolakkattu@live.ca>
2023-12-12 18:42:05 -0500

    added text

--- resources/views/pages/about/long.blade.php
+++ resources/views/pages/about/long.blade.php
@@ -1,3 +1,40 @@
 <div>
-    CHANGEME
+    <p>
+        As an accomplished Full Stack Developer, DevOps enthusiast, and AI aspirant, I hold a B.A. Honours in
+        Information Technology and a minor in Business Administration from York University. My experience spans across
+        software development, systems analysis, and project management, bolstered by a robust technical skillset in web
+        development, system administration, networking, and database creation and management. My hands-on experience
+        with Docker, Kubernetes, REST APIs, and Git, along with my proficiency in Java, Python, PHP, and other
+        programming languages, form the backbone of my technical abilities.
+    </p> <br />
+    <p>
+        One of my areas of expertise lies in managing the reliability, security, and integrity of large, cloud-based
+        Linux infrastructures. I've also successfully designed, developed, and managed databases, demonstrating my
+        comprehensive understanding of system administration and networking tasks. My knowledge extends to building web
+        servers on platforms like Raspberry Pi, further exhibiting my commitment to finding innovative solutions to
+        complex problems.
+    </p> <br />
+    <p>
+
+        Driven by an insatiable curiosity and a desire to make a difference, I continually explore the transformative
+        power of AI and advanced technologies. I am committed to staying abreast of industry trends, learning through
+        attending conferences, networking with peers, and independently exploring new technologies. This continuous
+        learning, coupled with my deep understanding of tech infrastructures and database management, enables me to
+        contribute significantly to the rapidly evolving tech landscape.
+    </p> <br />
+    <p>
+
+        My career journey is marked not only by my technical skills but also my ability to solve complex problems,
+        determination to succeed, and focus on creating value for customers, colleagues, and the community. I am a
+        natural collaborator and thrive in cross-functional teams, leveraging my strong communication skills to ensure
+        project success. Whether leading challenging software projects or participating in AI research initiatives, my
+        goal remains to make a positive impact, pushing the boundaries of what's possible.
+    </p> <br />
+    <p>
+
+        My ambition is to leverage my expertise to create better, more effective technologies that can benefit people
+        worldwide, within dynamic organizations. I am driven to build an incredible, profitable company that positively
+        impacts our customers, our employees, and our stakeholders, while also giving back to our communities. As I
+        forge ahead in this exciting industry, I am guided by a simple mantra – always learning, always improving.
+    </p>
 </div>
--- resources/views/pages/about/short.blade.php
+++ resources/views/pages/about/short.blade.php
@@ -1,3 +1,45 @@
 <div>
-    CHANGEME
+    As a Full Stack Developer and DevOps enthusiast, I leverage my comprehensive knowledge of software development, systems administration, and Agile methodologies to create cutting-edge solutions that impact people's lives and revolutionize industries. My continuous drive for learning, ability to solve complex problems, and commitment to community success define my journey in this fast-paced tech industry.
+    <x-modules.quick-links-box>
+        <x-modules.quick-links-box-item :menu="'About Me'" :item="'Long Version'">
+            <x-slot:linkText>
+            Itemlink
+            </x-slot:linkText>
+            <x-slot:description>
+                test123
+            </x-slot:description>
+        </x-modules.quick-links-box-item>
+        <x-modules.quick-links-box-item>
+            Item
+            <x-slot:description>
+                test
+            </x-slot:description>
+        </x-modules.quick-links-box-item>
+        <x-modules.quick-links-box-item>
+            Item
+            <x-slot:description>
+                test
+            </x-slot:description>
+        </x-modules.quick-links-box-item>
+        <x-modules.quick-links-box-item>
+            Item
+            <x-slot:description>
+                test
+            </x-slot:description>
+        </x-modules.quick-links-box-item>
+        <x-modules.quick-links-box-item>
+            Item
+            <x-slot:description>
+                test
+            </x-slot:description>
+        </x-modules.quick-links-box-item>
+        <x-modules.quick-links-box-item>
+            Item
+            <x-slot:description>
+                test
+            </x-slot:description>
+        </x-modules.quick-links-box-item>
+
+    </x-modules.quick-links-box>
+
 </div>
