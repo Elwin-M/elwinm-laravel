1d159bf98f9d62e8f7e5947111d18ceeb580c20e
Elwin <elwin-moolakkattu@live.ca>
2023-12-12 21:50:59 -0500

    added bar

--- resources/views/livewire/content-display.blade.php
+++ resources/views/livewire/content-display.blade.php
@@ -6,8 +6,13 @@
     <x-modules.hero-section />
 
     <div class="relative flex justify-center mx-auto max-w-8xl sm:px-2 lg:px-8 xl:px-12">
-        <!-- Left quicklinks -->
-        <x-modules.desktop-menu />
+        <div class="hidden lg:relative lg:block lg:flex-none">
+            <div class="absolute inset-y-0 right-0 w-[50vw] bg-slate-50 dark:hidden"></div>
+            <div class="absolute bottom-0 right-0 hidden w-px h-12 top-16 bg-gradient-to-t from-slate-800 dark:block"></div>
+            <div class="absolute bottom-0 right-0 hidden w-px top-28 bg-slate-800 dark:block"></div>
+            <!-- Left quicklinks -->
+            <x-modules.desktop-menu />
+        </div>
         <!-- Main body -->
         <div class="flex-auto max-w-2xl min-w-0 px-4 py-16 lg:max-w-none lg:pl-8 lg:pr-0 xl:px-16">
             <div class="space-y-1 mb-9">
