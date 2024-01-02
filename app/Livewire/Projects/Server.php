49f7f748f84b0e61c89a0371b4d9d1052094cc33
Elwin <elwin-moolakkattu@live.ca>
2024-01-02 14:07:51 -0500

    added base page

---
+++ app/Livewire/Projects/Server.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Projects;
+
+use Livewire\Component;
+
+class Server extends Component
+{
+    public function render()
+    {
+        return view('livewire.projects.server');
+    }
+}
