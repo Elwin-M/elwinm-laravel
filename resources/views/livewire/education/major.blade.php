a214cbd3db6c700991e1366f7d0c732b2840169f
Elwin <elwin-moolakkattu@live.ca>
2024-01-03 18:59:32 -0500

    education content

--- resources/views/livewire/education/major.blade.php
+++ resources/views/livewire/education/major.blade.php
@@ -1,12 +1,110 @@
-@section('pageTitle', 'Short')
+@section('pageTitle', 'Information Technology - Major')
 
 <div>
     <x-modules.title-subtitle>
         {{ $currentPageTitle }}
-        <x-slot:sub> {{ $currentPageSub }} </x-slot:sub>
+        <x-slot:sub> {{ $currentPageSub }} (2016 - 2021) </x-slot:sub>
     </x-modules.title-subtitle>
 
-    <p>
+    <p class="content-main-group">
         CHANGEME
     </p>
+    <!-- Start -->
+    <x-modules.description-list-main-outer>
+        <!-- First Box -->
+        <x-modules.description-list-main-inner>
+            <x-slot:title> Experience </x-slot:title>
+            <!-- List -->
+            <x-modules.description-list-item>
+                Acquired knowledge and skills in areas such as database design and management, web development, networking, and security
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Acquired knowledge of web server management and troubleshooting
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Developed a strong foundation in programming and software development principles and techniques
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Developed an understanding of knowledge management principles and practices, including knowledge acquisition, storage, retrieval, and dissemination
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Development of technical writing and presentation skills
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Experience with version control tools like Git and GitHub
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Experience with virtual machines and containerization using Docker and Oracle VirtualBox
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Explored the impact of information technology on organizational behaviour, including the effects of IT on organizational culture, structure, and strategy
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Gained experience working on group projects and collaborating with peers
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Gained exposure to various project management and team collaboration tools
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Gained knowledge and skills in the design and development of enterprise systems such as enterprise resource planning (ERP), supply chain management (SCM), customer relationship management (CRM), and business intelligence (BI)
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Learned about project management principles and practices as applied to information technology projects, including project initiation, planning, execution, control, and closure
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Learning about human-computer interaction principles and practices, including user interface design, usability testing, accessibility, and user experience (UX) design
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Studied the principles and practices of information technology security, including threat analysis, risk management, access control, cryptography, security policies, and security audits
+            </x-modules.description-list-item>
+        </x-modules.description-list-main-inner>
+        <!-- Second Box -->
+        <x-modules.description-list-main-inner>
+            <x-slot:title> Takeaways </x-slot:title>
+            <!-- List -->
+            <x-modules.description-list-item>
+                Communication skills, both written and verbal, with the ability to effectively communicate technical information to both technical and non-technical audiences
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Database design and management skills, with the ability to design and implement complex database systems
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Enterprise systems design and development skills, with the ability to analyze business processes and design and implement enterprise systems to improve organizational efficiency and effectiveness
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Experience working with various programming languages such as Java, Python, PHP, HTML/CSS
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Human-computer interaction design and development skills, with the ability to design effective and efficient user interfaces and evaluate their usability and user experience
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Knowledge management skills, with the ability to design and implement effective knowledge management systems
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Knowledge of database management systems including Oracle DBMS, and Microsoft Access
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Knowledge of various operating systems including Linux, Windows, and MacOS
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Problem-solving and critical thinking skills, with the ability to analyze complex problems and develop effective solutions
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Programming skills in languages such as Java, Python, and PHP
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Project management skills, with the ability to manage projects from initiation to closure, and to effectively manage project teams
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Skills in data analysis using tools such as Excel, MATLAB, and Python
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Skills in using various software tools like the Microsoft Suite, and Wireshark
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Understanding of systems design and architecture, as well as UML diagramming
+            </x-modules.description-list-item>
+        </x-modules.description-list-main-inner>
+    </x-modules.description-list-main-outer>
+    <!-- End -->
 </div>
--- resources/views/livewire/education/minor.blade.php
+++ resources/views/livewire/education/minor.blade.php
@@ -1,12 +1,53 @@
-@section('pageTitle', 'Short')
+@section('pageTitle', 'Business Administration - Minor')
 
 <div>
     <x-modules.title-subtitle>
         {{ $currentPageTitle }}
-        <x-slot:sub> {{ $currentPageSub }} </x-slot:sub>
+        <x-slot:sub> {{ $currentPageSub }} (2016 - 2021) </x-slot:sub>
     </x-modules.title-subtitle>
 
-    <p>
+    <p class="content-main-group">
         CHANGEME
     </p>
+    <!-- Start -->
+    <x-modules.description-list-main-outer>
+        <!-- First Box -->
+        <x-modules.description-list-main-inner>
+            <x-slot:title> Experience </x-slot:title>
+            <!-- List -->
+            <x-modules.description-list-item>
+                Broad understanding of how businesses operate and how to manage different aspects of a business
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Examining the fundamentals of management, including planning, organizing, leading, and controlling
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Exploring various marketing concepts such as segmentation, targeting, positioning, branding, and promotion
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Learning about the principles and practices of accounting, including financial statements, balance sheets, income statements, and cash flow statements
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Studying the basics of finance, including the time value of money, financial analysis, and risk management
+            </x-modules.description-list-item>
+        </x-modules.description-list-main-inner>
+        <!-- Second Box -->
+        <x-modules.description-list-main-inner>
+            <x-slot:title> Takeaways </x-slot:title>
+            <!-- List -->
+            <x-modules.description-list-item>
+                Create and manage a budget, assess investment opportunities, and analyze financial risks
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Designed and implemented effective marketing strategies that target specific customer segments
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                Management skills of people and resources to achieve organizational objectives
+            </x-modules.description-list-item>
+            <x-modules.description-list-item>
+                The ability to analyze financial statements and use financial ratios to evaluate a company's performance
+            </x-modules.description-list-item>
+        </x-modules.description-list-main-inner>
+    </x-modules.description-list-main-outer>
+    <!-- End -->
 </div>
