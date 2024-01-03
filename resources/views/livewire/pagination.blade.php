3244e595b24be663fb1bcb07c05990e9b741fc74
Elwin <elwin-moolakkattu@live.ca>
2024-01-03 17:11:38 -0500

    removed light mode and updated bar

--- resources/views/livewire/pagination.blade.php
+++ resources/views/livewire/pagination.blade.php
@@ -1,28 +1,33 @@
-<div class="flex pt-6 mt-12 border-t border-slate-200 dark:border-slate-800">
-    <div>
-        @if ($previousPage)
-            <div class="text-sm font-medium font-display text-slate-900 dark:text-white">
-                Previous
-            </div>
-            <div class="mt-1">
-                <a href="{{ $previousPage['uri'] }}"
-                    class="text-base font-semibold text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300" wire:navigate>
-                    <span aria-hidden="true">&larr;</span> {{ $previousPage['page'] }}
-                </a>
-            </div>
-        @endif
-    </div>
-    <div class="ml-auto text-right">
-        @if ($nextPage)
-            <div class="text-sm font-medium font-display text-slate-900 dark:text-white">
-                Next
-            </div>
-            <div class="mt-1">
-                <a href="{{ $nextPage['uri'] }}"
-                    class="text-base font-semibold text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300" wire:navigate>
-                    {{ $nextPage['page'] }} <span aria-hidden="true">&rarr;</span>
-                </a>
-            </div>
-        @endif
+<div>
+    <x-modules.bar-horizontal />
+    <div class="flex">
+        <div>
+            @if ($previousPage)
+                <div class="text-sm font-medium text-white font-display">
+                    Previous
+                </div>
+                <div class="mt-1">
+                    <a href="{{ $previousPage['uri'] }}"
+                        class="text-base font-semibold text-slate-400 hover:text-slate-300"
+                        wire:navigate>
+                        <span aria-hidden="true">&larr;</span> {{ $previousPage['page'] }}
+                    </a>
+                </div>
+            @endif
+        </div>
+        <div class="ml-auto text-right">
+            @if ($nextPage)
+                <div class="text-sm font-medium text-white font-display">
+                    Next
+                </div>
+                <div class="mt-1">
+                    <a href="{{ $nextPage['uri'] }}"
+                        class="text-base font-semibold text-slate-400 hover:text-slate-300"
+                        wire:navigate>
+                        {{ $nextPage['page'] }} <span aria-hidden="true">&rarr;</span>
+                    </a>
+                </div>
+            @endif
+        </div>
     </div>
 </div>
