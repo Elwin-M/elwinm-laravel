45594b4d974b112bafa127e6b3d7d8452fd9a72b
Elwin <elwin-moolakkattu@live.ca>
2024-01-08 01:52:22 -0500

    added resources to vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/fonts.css',
+                'resources/css/prism.css',
+                'resources/js/prism.js'
             ],
             refresh: [
                 ...refreshPaths,
