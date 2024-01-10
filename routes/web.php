ae9d0d5549b9e154aef413f8cd5b8b0ad5622cca
Elwin <elwin-moolakkattu@live.ca>
2024-01-09 20:26:23 -0500

    updated route names

--- routes/web.php
+++ routes/web.php
@@ -30,14 +30,14 @@ Route::get('/', Short::class)->name('elwinm/About Me/At A Glance');
 Route::get('/long', Long::class)->name('elwinm/About Me/Long Version');
 // Experience
 Route::get('/sfte-dev', Sfte::class)->name('elwinm/Experience/SFTE - Web Developer');
-Route::get('/stmarys-dev', Stmarys::class)->name('elwinm/Experience/St. Mary\'s - Full Stack Developer');
+Route::get('/stmary-dev', Stmarys::class)->name('elwinm/Experience/St. Mary\'s - Full Stack Developer');
 Route::get('/elwinm-dev', Elwinm::class)->name('elwinm/Experience/ElwinM - Full Stack Developer');
 Route::get('/elwinm-sys-admin', Server::class)->name('elwinm/Experience/ElwinM - System Administrator');
 // Education
 Route::get('/major', Major::class)->name('elwinm/Education (York University)/B.A. Hons: Information Technology');
 Route::get('/minor', Minor::class)->name('elwinm/Education (York University)/Minor: Business Administration');
 // Projects
-Route::get('/elwinm-old', ElwinmOld::class)->name('elwinm/Projects/ElwinM Old Website');
+Route::get('/elwinm-old', ElwinmOld::class)->name('elwinm/Projects/ElwinM Old Website And Server');
 Route::get('/projects', Random::class)->name('elwinm/Projects/Assorted Projects And Scripts');
 // Skills And Tools
 Route::get('/skills-tools', SkillsTools::class)->name('elwinm/Skill And Tools/Skills And Tools');
