4914097e24fe95acf26865f1febd304a66e663b0
Elwin <elwin-moolakkattu@live.ca>
2024-01-12 00:02:17 -0500

    changed dropdown styling to be a box

--- resources/views/components/modules/dropdown.blade.php
+++ resources/views/components/modules/dropdown.blade.php
@@ -1,7 +1,7 @@
-<div x-cloak class="pt-4" x-data="{ dropdown: false }">
+<div x-cloak class="px-6 border rounded-md shadow-sm border-slate-800" x-data="{ dropdown: false }">
     <!-- Expand/collapse -->
     <div @click="dropdown = !dropdown" class="hover:cursor-pointer">
-        <div class="flex items-start justify-between w-full py-4 text-left text-white border-t border-slate-800"
+        <div class="flex items-start justify-between w-full py-4 text-left text-white"
             aria-controls="faq-0" aria-expanded="false">
             <span class="text-base font-semibold leading-7">{{ $slot }}</span>
             <span class="flex items-center ml-6 h-7">
@@ -17,7 +17,7 @@
         </div>
     </div>
     <!-- Actual dropdown -->
-    <div x-show="dropdown" class="px-4 mt-2">
+    <div x-show="dropdown" class="mt-2">
         {{ $content }}
     </div>
 </div>
